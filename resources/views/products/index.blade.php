dbe43c604922249b1198fa9f756fa19c240268c1
EsraaKedwany <esraa.mohamed1006@gmail.com>
2019-07-19 14:56:54 +0200

    Create product controller

---
+++ resources/views/products/index.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.base')
+@section('content')
+
+<img src="images/home/main-image.png" width="1299" height="578" alt="">
+
+
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,6 @@
 Route::get('/', function () {
     return view('home');
 });
+
+//Products Routes
+Route::get('/products','ProductController@index');
